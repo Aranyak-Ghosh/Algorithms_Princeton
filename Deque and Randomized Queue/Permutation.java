42c4c928d2518cda7c36c3081607aa08e028fbf6
Aranyak-Ghosh <h_abbasi97@hotmail.com>
2019-04-19 01:40:02 +0400

    Read all inputs from stdin

--- Deque and Randomized Queue/Permutation.java
+++ Deque and Randomized Queue/Permutation.java
@@ -4,13 +4,13 @@ import edu.princeton.cs.algs4.StdOut;
 public class Permutation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		RandomizedQueue<String> q = new RandomizedQueue<String>();
 		int count = Integer.parseInt(args[0]);
-		while (StdIn.hasNextLine() && !StdIn.isEmpty()) {
+		while (!StdIn.isEmpty() && count > 0) {
 			q.enqueue(StdIn.readString());
+			count--;
 		}
-		while (count > 0) {
+		while (!q.isEmpty()) {
 			StdOut.println(q.dequeue());
 			count--;
 		}
