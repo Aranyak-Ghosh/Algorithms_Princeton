ad05a0c2c82310582db7eccd17fc3859c8c49212
Aranyak-Ghosh <aranyakghosh@gmail.com>
2019-04-25 13:15:42 +0400

    Implemented A* search for solution

--- 8 Puzzle/Solver.java
+++ 8 Puzzle/Solver.java
@@ -19,7 +19,7 @@ public class Solver {
 	}
 
 	private final MinPQ<Node> heap;
-	private final MinPQ<Node> twin_heap;
+	private final MinPQ<Node> heap_twin;
 	private final Stack<Board> solution;
 	private boolean solvable;
 
@@ -47,30 +47,63 @@ public class Solver {
 	public Solver(Board initial) {
 		// TODO Auto-generated constructor stub
 		solvable = true;
-		solution = new Stack();
+		solution = new Stack<Board>();
 		if (initial == null)
 			throw new IllegalArgumentException();
 
 		Node node = new Node(initial, 0, null);
 		Node node_twin = new Node(initial.twin(), 0, null);
 		heap = new MinPQ<Node>(new Priority());
-		twin_heap = new MinPQ<Node>(new Priority());
+		heap_twin = new MinPQ<Node>(new Priority());
 		heap.insert(node);
-		twin_heap.insert(node_twin);
+		heap_twin.insert(node_twin);
 
 		while (!heap.isEmpty()) {
-			node_twin = twin_heap.delMin();
+			node_twin = heap_twin.delMin();
 			if (node_twin.board.isGoal()) {
 				solvable = false;
 				break;
 			}
-			
-			node=heap.delMin();
-			if(node.board.isGoal())
+
+			node = heap.delMin();
+			if (node.board.isGoal())
 				generateStack(node);
 			else {
-				Node child,twin_child;
+				Node child, child_twin;
+				Iterable<Board> neighbors = node.board.neighbors();
+				Iterable<Board> neighbors_twin = node_twin.board.neighbors();
+				boolean addChild, addChild_twin;
+				addChild = true;
+				addChild_twin = true;
+				for (Board b : neighbors) {
+					if (addChild) {
+						if (node.parent != null && node.board.equals(b)) {
+							addChild = false;
+							continue;
+						} else {
+							child = new Node(b, node.steps + 1, node);
+							heap.insert(child);
+						}
+					} else {
+						child = new Node(b, node.steps + 1, node);
+						heap.insert(child);
+					}
+				}
 				
+				for (Board b : neighbors_twin) {
+					if (addChild_twin) {
+						if (node_twin.parent != null && node_twin.board.equals(b)) {
+							addChild_twin = false;
+							continue;
+						} else {
+							child_twin = new Node(b, node_twin.steps + 1, node_twin);
+							heap_twin.insert(child_twin);
+						}
+					} else {
+						child_twin = new Node(b, node_twin.steps + 1, node_twin);
+						heap_twin.insert(child_twin);
+					}
+				}
 			}
 
 		}
